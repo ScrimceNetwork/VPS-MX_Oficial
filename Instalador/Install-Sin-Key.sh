a6072b9df64521b974ebc24f21f86d4115c655d6
ScrimceNetwork <108626411+ScrimceNetwork@users.noreply.github.com>
2022-08-22 11:31:35 -0500

    Update Install-Sin-Key.sh

--- Instalador/Install-Sin-Key.sh
+++ Instalador/Install-Sin-Key.sh
@@ -407,7 +407,7 @@ install_ADMRufu() {
   clear && clear
   mkdir /etc/ADMRufu >/dev/null 2>&1
   cd /etc
-  wget https://raw.githubusercontent.com/ScrimceNetwork/Multi-Script/main/R9/ADMRufu.tar.xz >/dev/null 2>&1
+  wget https://github.com/ScrimceNetwork/VPS-MX_Oficial/blob/master/ADMRufu/ADMRufu.tar.xz >/dev/null 2>&1
   tar -xf ADMRufu.tar.xz >/dev/null 2>&1
   chmod +x ADMRufu.tar.xz >/dev/null 2>&1
   rm -rf ADMRufu.tar.xz
